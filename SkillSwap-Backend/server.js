0b3b3da629ac1e40468b49a222a3eeb09b0e5f87
dhivyarackshana.sridharan@ontariotechu.net <dhivyarackshana.sridharan@ontariotechu.net>
2025-11-16 18:02:07 -0500

    Finalized backend & frontend env setup, updated README

--- SkillSwap-Backend/server.js
+++ SkillSwap-Backend/server.js
@@ -18,12 +18,9 @@ app.use(express.json());      // Parse JSON bodies
 
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log(' Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 
 // Basic route to test server
