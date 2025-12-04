1f828ccd001d3dee86393870bf070a09a8db06f2
DhivyarackshanaSridharan <dhivyarackshana.sridharan@ontariotechu.net>
2025-12-04 00:16:19 -0500

    Add new allowed origin for CORS

--- SkillSwap-Backend/server.js
+++ SkillSwap-Backend/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   'http://localhost:3001',
   'https://frontend-fp7sqranu-dhivyarackshanasridharans-projects.vercel.app',
   'https://infr-3120-fall25-project-eight.vercel.app',
-  'https://ssfrontend-a70c30uew-dhivyarackshanasridharans-projects.vercel.app'
+  'https://ssfrontend-orcin.vercel.app'
 ];
 
 const corsOptions = {
